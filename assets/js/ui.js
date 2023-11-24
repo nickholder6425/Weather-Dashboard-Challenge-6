bdae7d7147f91cb0de0659c2ef8607eb0e437659
nicholasholder <ngholder@hotmail.com>
2023-11-24 14:23:40 -0600

    deleted js folders

--- assets/js/ui.js
+++
@@ -1,7 +0,0 @@
-// Module for updating UI with weather data
-export function updateUI(data) {
-    const weatherDataElement = document.getElementById('weather-data');
-    // Use the retrieved data to update the weather dashboard
-    // Example: weatherDataElement.innerHTML = JSON.stringify(data, null, 2);
-  }
-  
